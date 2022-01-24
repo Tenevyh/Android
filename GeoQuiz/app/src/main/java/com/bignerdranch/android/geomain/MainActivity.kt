ab9fc41b9e324321cd1881efe0a45633cd55ea5e
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-01-24 22:10:57 +0300

    update

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -2,7 +2,6 @@ package com.bignerdranch.android.geomain
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
