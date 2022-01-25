c4eb97b1f71511a164d2a2d83782a14d3faa9d0e
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-01-26 00:11:49 +0300

    add ImageButton Next and Prev

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
@@ -11,7 +12,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -32,6 +34,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
@@ -39,17 +42,30 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        questionTextView.setOnClickListener{view: View ->
+            if(currentIndex==questionBank.size-1){} else {
+                currentIndex = (currentIndex + 1) % questionBank.size
+                updateQuestion()
+            }
+        }
 
         falseButton.setOnClickListener { view: View ->
             checkAnswer(false)
         }
 
-        nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
-            updateQuestion()
+        prevButton.setOnClickListener {
+            if(currentIndex==0){} else {
+                currentIndex = (currentIndex - 1) % questionBank.size
+                updateQuestion()
+            }
         }
 
-        updateQuestion()
+        nextButton.setOnClickListener {
+            if(currentIndex==questionBank.size-1){} else {
+                currentIndex = (currentIndex + 1) % questionBank.size
+                updateQuestion()
+            }
+        }
 
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
