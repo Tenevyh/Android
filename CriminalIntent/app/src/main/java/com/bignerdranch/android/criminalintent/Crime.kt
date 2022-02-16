c726f480718fe2d53c285510df696b477fa66555
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-02-16 22:57:18 +0300

    create data class Crime

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -1,4 +1,10 @@
 package com.bignerdranch.android.criminalintent
 
-class Crime {
+import java.util.*
+
+data class Crime (val id: UUID = UUID.randomUUID(),
+                  var title: String ="",
+                  val date: Date = Date(),
+                  var isSolved: Boolean = false) {
+
 }
