6ec700399de85545e5a9bb13dabdf4198e64c7f7
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-02-17 21:13:49 +0300

    Update CrimeFragment.kt

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -1,7 +1,23 @@
 package com.bignerdranch.android.criminalintent
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
 
 class CrimeFragment: Fragment() {
+    private lateinit var crime: Crime
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime=Crime()
+
+    }
+
+    override fun onCrateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+        return view
+    }
 }
