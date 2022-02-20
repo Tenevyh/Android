05029669234b0d391a4ad8ce7c6e987129efa495
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-02-20 17:46:05 +0300

    Create CrimeListFragment.kt

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -0,0 +1,29 @@
+package com.bignerdranch.android.criminalintent
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
+import androidx.lifecycle.ViewModelProviders
+
+
+private const val TAG = "CrimeListFragment"
+
+class CrimeListFragment: Fragment() {
+
+    private val crimeListViewModel: CrimeListViewModel by lazy{
+        ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
+    }
+
+    companion object {
+        fun newInstance(): CrimeListFragment{
+            return CrimeListFragment()
+        }
+    }
+}
+
+
