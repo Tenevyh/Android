0666cde6079d00a8e373068a110cc8c80e3b15c2
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-03-07 13:29:49 +0300

    Update CrimeDataBase.kt

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDataBase.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDataBase.kt
@@ -6,7 +6,7 @@ import androidx.room.TypeConverter
 import androidx.room.TypeConverters
 import com.bignerdranch.android.criminalintent.Crime
 
-@Database(entities = [Crime::class], version = 1)
+@Database(entities = [Crime::class], version = 1, exportSchema = false)
 @TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDataBase : RoomDatabase(){
 
