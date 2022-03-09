abb01a76bcb72f87c7775173d8814c77436db09c
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-03-10 00:33:43 +0300

    update

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.bignerdranch.android.criminalintent
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity(),
+    CrimeListFragment.Callbacks {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
 
-        if(currentFragment == null){
+        if (currentFragment == null) {
             val fragment = CrimeListFragment.newInstance()
             supportFragmentManager.beginTransaction()
                 .add(R.id.fragment_container, fragment)
                 .commit()
         }
+    }
+
+    override fun onCrimeSelected(crimeId: UUID) {
+        Log.d(TAG, "MainActivity.onCrimeSelected: $crimeId")
     }
 }
