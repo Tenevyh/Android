b5c37e6edf9d5850184066bdaa8a4e623299e8eb
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-03-24 23:11:19 +0300

    Update DatePickerFragment.kt

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import java.util.*
 
+private const val ARG_DATE = "date"
+
 class DatePickerFragment: DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -19,4 +21,14 @@ class DatePickerFragment: DialogFragment() {
         )
     }
 
+    companion object{
+        fun newInstance(date: Date): DatePickerFragment{
+            val args = Bundle().apply {
+                putSerializable(ARG_DATE, date)
+            }
+            return DatePickerFragment().apply {
+                arguments = args
+            }
+        }
+    }
 }
