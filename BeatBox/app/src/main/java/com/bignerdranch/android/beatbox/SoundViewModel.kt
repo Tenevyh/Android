18125214d50b6332de7b23c19e06dae9b0a5d050
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-05-07 23:42:51 +0300

    Binding to the ViewModel

---
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.kt
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.beatbox
+
+class SoundViewModel {
+
+    var sound: Sound? = null
+    set(sound) {
+        field = sound
+    }
+
+    val title: String?
+    get()= sound?.name
+}
