42fe6e2c8b361e28ed9d391c3aaddbe0c26e680f
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-06-10 23:36:06 +0300

    add ViewModel and update RecyclerView

--- PhotoGallery/app/src/main/java/com/project/android/photogallery/PhotoGalleryViewModel.kt
+++ PhotoGallery/app/src/main/java/com/project/android/photogallery/PhotoGalleryViewModel.kt
@@ -1,13 +1,12 @@
 package com.project.android.photogallery
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
 
-class PhotoGalleryViewModel {
+class PhotoGalleryViewModel: ViewModel() {
     val galleryItemLiveData: LiveData<List<GalleryItem>>
 
     init {
-
         galleryItemLiveData = FlickrFetchr().fetchPhotos()
-
         }
 }
