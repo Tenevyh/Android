1baf1bc6e78bdc2875d31cc73891770b87f60715
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-06-20 21:11:38 +0300

    add search

--- PhotoGallery/app/src/main/java/com/project/android/photogallery/FlickrFetchr.kt
+++ PhotoGallery/app/src/main/java/com/project/android/photogallery/FlickrFetchr.kt
@@ -6,15 +6,15 @@ import android.util.Log
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.project.android.photogallery.api.FlickrApi
 import com.project.android.photogallery.api.FlickrResponse
+import com.project.android.photogallery.api.PhotoInterceptor
 import com.project.android.photogallery.api.PhotoResponse
+import okhttp3.OkHttpClient
 import okhttp3.ResponseBody
 import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
 
 private const val TAG = "FlickrFetchr"
 
@@ -23,40 +23,50 @@ class FlickrFetchr {
     private val flickrApi: FlickrApi
 
     init {
-        val gsonPhotoDeserializer = GsonBuilder()
-            .registerTypeAdapter(PhotoResponse::class.java, PhotoDeserializer())
-            .create()
+        val client = OkHttpClient.Builder().addInterceptor(PhotoInterceptor()).build()
 
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl("https://api.flickr.com/")
-            .addConverterFactory(GsonConverterFactory.create(gsonPhotoDeserializer)).build()
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client).build()
 
         flickrApi = retrofit.create(FlickrApi::class.java)
     }
 
-    fun fetchPhotos(): LiveData<List<GalleryItem>>{
+    fun fetchPhotos(): LiveData<List<GalleryItem>> {
+        return fetchPhotoMetadata(flickrApi.fetchPhotos())
+    }
+
+    fun searchPhotos(query: String): LiveData<List<GalleryItem>> {
+        return fetchPhotoMetadata(flickrApi.searchPhotos(query))
+    }
+
+    private fun fetchPhotoMetadata(flickrRequest: Call<FlickrResponse>)
+    : LiveData<List<GalleryItem>>{
         val responseLiveData: MutableLiveData<List<GalleryItem>> = MutableLiveData()
-        val flickrRequest: Call<PhotoDeserializer> = flickrApi.fetchPhotos()
 
 
-        flickrRequest.enqueue(object : Callback<PhotoDeserializer> {
-            override fun onFailure(call: Call<PhotoDeserializer>, t: Throwable){
+        flickrRequest.enqueue(object : Callback<FlickrResponse> {
+
+            override fun onFailure(call: Call<FlickrResponse>, t: Throwable) {
                 Log.e(TAG, "Failed to fetch photos", t)
             }
 
-            override fun onResponse(call: Call<PhotoDeserializer>, response: Response<PhotoDeserializer>) {
-                Log.d(TAG, "Response received: ${response.body()}")
-                val flickrResponse: PhotoDeserializer? = response.body()
+            override fun onResponse(call: Call<FlickrResponse>, response: Response<FlickrResponse>) {
+                Log.d(TAG, "Response received")
+                val flickrResponse: FlickrResponse? = response.body()
                 val photoResponse: PhotoResponse? = flickrResponse?.photos
-                var galleryItems: List<GalleryItem> = photoResponse?.galleryItems?: mutableListOf()
-                galleryItems = galleryItems.filterNot{
+                var galleryItems: List<GalleryItem> = photoResponse?.galleryItems
+                    ?: mutableListOf()
+                galleryItems = galleryItems.filterNot {
                     it.url.isBlank()
                 }
-               responseLiveData.value = galleryItems
+                responseLiveData.value = galleryItems
             }
         })
+
         return responseLiveData
-        }
+    }
 
     @WorkerThread
     fun fetchPhoto(url: String): Bitmap? {
--- PhotoGallery/app/src/main/java/com/project/android/photogallery/api/FlickrApi.kt
+++ PhotoGallery/app/src/main/java/com/project/android/photogallery/api/FlickrApi.kt
@@ -1,30 +1,19 @@
 package com.project.android.photogallery.api
 
-import com.project.android.photogallery.PhotoDeserializer
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 import retrofit2.http.Url
 
 interface FlickrApi {
 
-    @GET(
-
-        "services/rest/?method=flickr.interestingness.getList" +
-
-    "&api_key=c7d5609553a4d9560b5b29dfd57e483b" +
-
-    "&format=json" +
-
-    "&nojsoncallback=1" +
-
-    "&extras=url_s"
-
-    )
-
-   fun fetchPhotos(): Call<PhotoDeserializer>
+    @GET("services/rest?method=flickr.interestingness.getList")
+   fun fetchPhotos(): Call<FlickrResponse>
 
    @GET
    fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
 
+   @GET("services/rest?method=flickr.photos.search")
+   fun searchPhotos(@Query("text") query: String): Call<FlickrResponse>
 }
