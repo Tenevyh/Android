a03668985b334ce77d7465c55bcd4c4e91c307f7
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-06-22 22:16:32 +0300

    Update PhotoGalleryViewModel.kt

--- PhotoGallery/app/src/main/java/com/project/android/photogallery/PhotoGalleryViewModel.kt
+++ PhotoGallery/app/src/main/java/com/project/android/photogallery/PhotoGalleryViewModel.kt
@@ -1,13 +1,24 @@
 package com.project.android.photogallery
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class PhotoGalleryViewModel : ViewModel() {
 
     val galleryItemLiveData: LiveData<List<GalleryItem>>
+    private val flickrFetchr = FlickrFetchr()
+    private val mutableSearchTerm = MutableLiveData<String>()
 
     init {
-        galleryItemLiveData = FlickrFetchr().searchPhotos("planets")
+        galleryItemLiveData = Transformations.switchMap(mutableSearchTerm) {
+            searchTerm -> flickrFetchr.searchPhotos(searchTerm)
+        }
     }
+
+    fun fetchPhotos(query: String = "") {
+        mutableSearchTerm.value = query
+    }
+
 }
