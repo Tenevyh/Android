bdf8e1cb2bb88f669d02da97f9e242f6f3a3b101
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-07-05 21:33:31 +0300

    Update FlickrApi.kt

--- PhotoGallery/app/src/main/java/com/tenevyh/android/photogallery/FlickrApi/FlickrApi.kt
+++ PhotoGallery/app/src/main/java/com/tenevyh/android/photogallery/FlickrApi/FlickrApi.kt
@@ -2,7 +2,15 @@ package com.tenevyh.android.photogallery.FlickrApi
 
 import retrofit2.http.GET
 
+private const val API_KEY = "c7d5609553a4d9560b5b29dfd57e483b"
+
 interface FlickrApi {
-    @GET("/")
-    suspend fun fetchContents(): String
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList"+
+                "&api_key=$API_KEY"+
+                "&format=json"+
+                "&nojsoncallback=1"+
+                "&extras=url_s"
+    )
+    suspend fun fetchPhotos(): String
 }
