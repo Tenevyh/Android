e6645987862d4c5368f6ab693477eb0270af38ab
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-07-12 21:39:42 +0300

    Update PhotoListAdapter.kt

--- PhotoGallery/app/src/main/java/com/tenevyh/android/photogallery/PhotoListAdapter.kt
+++ PhotoGallery/app/src/main/java/com/tenevyh/android/photogallery/PhotoListAdapter.kt
@@ -14,16 +14,18 @@ class PhotoViewHolder(private val binding: ListItemGalleryBinding)
     }
 }
 
-class PhotoListAdapter: RecyclerView.Adapter<PhotoViewHolder>() {
+class PhotoListAdapter(private val galleryItems: List<GalleryItem>)
+    : RecyclerView.Adapter<PhotoViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
-        return PhotoViewHolder()
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ListItemGalleryBinding.inflate(inflater, parent, false)
+        return PhotoViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = galleryItems[position]
+        holder.bind(item)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = galleryItems.size
 }
