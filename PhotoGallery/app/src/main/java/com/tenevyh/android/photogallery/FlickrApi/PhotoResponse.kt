b29a9331372c94af566fead82f9792ed0ebf01ca
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-07-17 21:15:11 +0300

    add Interceptor

--- PhotoGallery/app/src/main/java/com/tenevyh/android/photogallery/FlickrApi/PhotoResponse.kt
+++ PhotoGallery/app/src/main/java/com/tenevyh/android/photogallery/FlickrApi/PhotoResponse.kt
@@ -5,5 +5,5 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class PhotoResponse (
-    @Json(name = "photo") val galleryItem: List<GalleryItem>
+    @Json(name = "photo") val galleryItems: List<GalleryItem>
         )
