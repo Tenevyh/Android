8029a09aff67e14a62cd3dd067c45591d21d4fb2
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-07-26 21:18:29 +0300

    add Animation

--- Sunset/app/src/main/java/com/tenevyh/android/sunset/MainActivity.kt
+++ Sunset/app/src/main/java/com/tenevyh/android/sunset/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.tenevyh.android.sunset
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AccelerateInterpolator
+import com.tenevyh.android.sunset.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.scene.setOnClickListener{
+            startAnimation()
+        }
+    }
+
+    private fun startAnimation() {
+        val sunYStart = binding.sun.top.toFloat()
+        val sunXEnd = binding.sky.height.toFloat()
+
+        val heightAnimator = ObjectAnimator
+            .ofFloat(binding.sun, "y", sunYStart, sunXEnd)
+            .setDuration(3000)
+        heightAnimator.interpolator = AccelerateInterpolator()
+
+        heightAnimator.start()
     }
 }
