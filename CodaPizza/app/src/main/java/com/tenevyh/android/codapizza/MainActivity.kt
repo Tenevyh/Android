7b86d180d634c19e1c2a4c8b6da30cbe44d3274e
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-07-30 01:29:27 +0300

    update MainActivity

--- CodaPizza/app/src/main/java/com/tenevyh/android/codapizza/MainActivity.kt
+++ CodaPizza/app/src/main/java/com/tenevyh/android/codapizza/MainActivity.kt
@@ -3,13 +3,24 @@ package com.tenevyh.android.codapizza
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hello World")
+            Row {
+                Checkbox(checked = true,
+                    onCheckedChange = {/*TODO */ })
+                Column {
+                    Text(text = "Pineapple")
+                    Text(text = "Whole pizza")
+                }
+            }
         }
     }
 }
