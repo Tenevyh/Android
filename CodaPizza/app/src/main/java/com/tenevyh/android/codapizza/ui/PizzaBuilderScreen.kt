c1f3323aaf300e0734f0c747c709d3329f278e38
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-08-06 19:08:17 +0300

    savedInstanceState

--- CodaPizza/app/src/main/java/com/tenevyh/android/codapizza/ui/PizzaBuilderScreen.kt
+++ CodaPizza/app/src/main/java/com/tenevyh/android/codapizza/ui/PizzaBuilderScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.intl.Locale
@@ -20,36 +21,45 @@ import com.tenevyh.android.codapizza.R
 import com.tenevyh.android.codapizza.model.Pizza
 import com.tenevyh.android.codapizza.model.Topping
 import com.tenevyh.android.codapizza.model.ToppingPlacement
+import java.text.NumberFormat
 
 @Preview
 @Composable
 fun PizzaBuilderScreen(modifier: Modifier = Modifier){
+    var pizza by rememberSaveable(Pizza()) {
+        mutableStateOf(Pizza())
+    }
     Column(modifier = modifier) {
-        ToppingsList(modifier = Modifier
-            .fillMaxWidth()
-            .weight(1f, fill = true))
-        OrderButton(modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp))
+        ToppingsList(
+            pizza = pizza,
+            onEditPizza = {pizza = it},
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f, fill = true))
+        OrderButton(pizza = pizza,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp))
     }
 }
 
 @Composable
-private fun ToppingsList(modifier: Modifier = Modifier){
-    var pizza by remember { mutableStateOf(Pizza())}
+private fun ToppingsList(pizza: Pizza, onEditPizza: (Pizza) -> Unit,
+                         modifier: Modifier = Modifier){
+
     LazyColumn(modifier = modifier){
         items(Topping.values()) { topping ->
             ToppingCell(topping = topping,
                 placement = pizza.toppings[topping],
                 onClickTopping = {val isOnPizza = pizza.toppings[topping] != null
-                    pizza = pizza.withTopping(
+                    onEditPizza(pizza.withTopping(
                         topping = topping,
                         placement = if (isOnPizza) {
                             null
                         } else {
                             ToppingPlacement.All
                         }
-                    )
+                    ))
                 }
             )
         }
@@ -57,9 +67,16 @@ private fun ToppingsList(modifier: Modifier = Modifier){
 }
 
 @Composable
-private fun OrderButton(modifier: Modifier = Modifier){
+private fun OrderButton(
+    pizza: Pizza,
+    modifier: Modifier = Modifier){
     Button(modifier = modifier,
         onClick = { /*TODO*/ }) {
-        Text(text = stringResource(R.string.place_order_button).toUpperCase(Locale.current))
+        val currencyFormatter = remember{NumberFormat.getCurrencyInstance()}
+        val price = currencyFormatter.format(pizza.price)
+        Text(
+            text = stringResource(R.string.place_order_button, price)
+                .toUpperCase(Locale.current)
+        )
     }
 }
