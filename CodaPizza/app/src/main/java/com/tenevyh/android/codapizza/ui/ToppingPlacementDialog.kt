358374e2f7211f7e5ab612d7f8b87dc3869573a6
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-08-07 21:55:07 +0300

    Update ToppingPlacementDialog.kt

--- CodaPizza/app/src/main/java/com/tenevyh/android/codapizza/ui/ToppingPlacementDialog.kt
+++ CodaPizza/app/src/main/java/com/tenevyh/android/codapizza/ui/ToppingPlacementDialog.kt
@@ -1,22 +1,23 @@
 package com.tenevyh.android.codapizza.ui
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.tenevyh.android.codapizza.R
 import com.tenevyh.android.codapizza.model.Topping
+import com.tenevyh.android.codapizza.model.ToppingPlacement
 
 @Composable
 fun ToppingPlacementDialog(topping: Topping,
@@ -31,5 +32,20 @@ fun ToppingPlacementDialog(topping: Topping,
                     modifier = Modifier.padding(24.dp))
             }
         }
+    }
+}
+
+@Composable
+private fun ToppingPlacementOption(@StringRes placementName: Int,
+                                   onClick: () -> Unit,
+                                   modifier: Modifier = Modifier){
+    TextButton(
+        onClick = onClick,
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = stringResource(placementName),
+            modifier = Modifier.padding(8.dp)
+        )
     }
 }
