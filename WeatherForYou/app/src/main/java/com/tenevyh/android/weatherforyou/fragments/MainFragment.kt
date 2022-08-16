75a60761febd99f08be1d7f9c401903e8846c51f
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-08-16 20:46:06 +0300

    add check permission

--- WeatherForYou/app/src/main/java/com/tenevyh/android/weatherforyou/fragments/MainFragment.kt
+++ WeatherForYou/app/src/main/java/com/tenevyh/android/weatherforyou/fragments/MainFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.tenevyh.android.weatherforyou.databinding.FragmentMainBinding
+import java.util.jar.Manifest
 
 class MainFragment : Fragment() {
 
+    private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
@@ -17,6 +22,24 @@ class MainFragment : Fragment() {
     ): View? {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    private fun permissionListener(){
+        pLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun checkPermission(){
+        if(!isPermissionGranted(android.Manifest.permission.ACCESS_FINE_LOCATION)){
+            permissionListener()
+            pLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
     companion object {
