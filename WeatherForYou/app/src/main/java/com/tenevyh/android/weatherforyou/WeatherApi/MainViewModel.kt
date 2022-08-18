a83438cc78702fe1a06073c088972c6f4969d947
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-08-18 20:24:01 +0300

    add item for recyclerView

--- WeatherForYou/app/src/main/java/com/tenevyh/android/weatherforyou/WeatherApi/MainViewModel.kt
+++ WeatherForYou/app/src/main/java/com/tenevyh/android/weatherforyou/WeatherApi/MainViewModel.kt
@@ -2,8 +2,9 @@ package com.tenevyh.android.weatherforyou.WeatherApi
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.tenevyh.android.weatherforyou.adapters.WeatherModel
 
 class MainViewModel: ViewModel(){
-    val liveDataCurrent = MutableLiveData<String>()
-    val liveDataList = MutableLiveData<List<String>>()
+    val liveDataCurrent = MutableLiveData<WeatherModel>()
+    val liveDataList = MutableLiveData<List<WeatherModel>>()
 }
--- WeatherForYou/app/src/main/java/com/tenevyh/android/weatherforyou/fragments/MainFragment.kt
+++ WeatherForYou/app/src/main/java/com/tenevyh/android/weatherforyou/fragments/MainFragment.kt
@@ -8,11 +8,17 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.tenevyh.android.weatherforyou.adapters.VpAdapter
 import com.tenevyh.android.weatherforyou.databinding.FragmentMainBinding
 import java.util.jar.Manifest
 
 class MainFragment : Fragment() {
 
+    private val tList = listOf("HOURS", "DAYS")
+    private val fList = listOf(HoursFragment.newInstance(), DaysFragment.newInstance())
     private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
 
@@ -27,6 +33,15 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
+        init()
+    }
+
+    private fun init() = with(binding){
+        val adapter = VpAdapter(activity as FragmentActivity, fList)
+        vp.adapter = adapter
+        TabLayoutMediator(tabLayout, vp){
+            tab, pos -> tab.text = tList[pos]
+        }.attach()
     }
 
     private fun permissionListener(){
