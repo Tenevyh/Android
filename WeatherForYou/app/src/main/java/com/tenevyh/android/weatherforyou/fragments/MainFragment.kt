c295a0f17f77891a5ee5da87191e13541c3075f7
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-08-20 22:22:06 +0300

    Update MainFragment.kt

--- WeatherForYou/app/src/main/java/com/tenevyh/android/weatherforyou/fragments/MainFragment.kt
+++ WeatherForYou/app/src/main/java/com/tenevyh/android/weatherforyou/fragments/MainFragment.kt
@@ -16,7 +16,9 @@ import com.android.volley.toolbox.Volley
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.tenevyh.android.weatherforyou.adapters.VpAdapter
+import com.tenevyh.android.weatherforyou.adapters.WeatherModel
 import com.tenevyh.android.weatherforyou.databinding.FragmentMainBinding
+import org.json.JSONObject
 import java.util.jar.Manifest
 
 const val API_KEY = "f9724f9d686549189fb80258221308"
@@ -69,16 +71,50 @@ class MainFragment : Fragment() {
         val queue = Volley.newRequestQueue(context)
         val request = StringRequest(
             Request.Method.GET, url,
-            {result -> Log.d("MyLog", "Result: $result")},
+            {result -> parseWeatherData(result)},
             {error -> Log.d("MyLog", "Error: $error")}
         )
         queue.add(request)
+    }
+
+    private fun parseWeatherData(result: String){
+        val mainObject = JSONObject(result)
+        parseCurrentData(mainObject)
+    }
+
+    private fun parseCurrentData(mainObject: JSONObject){
+        val item = WeatherModel(
+            mainObject.getJSONObject("location").getString("name"),
+            mainObject.getJSONObject("current").getString("last_updated"),
+            mainObject.getJSONObject("current")
+                .getJSONObject("condition").getString("text"),
+            mainObject.getJSONObject("current").getString("temp_c"),
+            "",
+            "",
+            mainObject.getJSONObject("current")
+                .getJSONObject("condition").getString("icon"),
+            ""
+        )
+        Log.d("MyLog", "City: ${item.city}")
+        Log.d("MyLog", "Time: ${item.time}")
+        Log.d("MyLog", "Condition: ${item.condition}")
+        Log.d("MyLog", "CurrentTemp: ${item.currentTemp}")
+        Log.d("MyLog", "URL: ${item.imageURL}")
     }
 
     private fun checkPermission(){
         if(!isPermissionGranted(android.Manifest.permission.ACCESS_FINE_LOCATION)){
             permissionListener()
             pLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    private fun parseDays(mainObject: JSONObject) : List<WeatherModel>{
+        val list = ArrayList<WeatherModel>()
+        val daysArray = mainObject.getJSONObject("forecast")
+            .getJSONArray("forecastday")
+        for(i in 0 until daysArray.length()){
+            val day = daysArray[i] as JSONObject
         }
     }
 
