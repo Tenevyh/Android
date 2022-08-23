591c7edac6f7a595f5ac96f8e0620cc06e449e9b
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-08-23 20:57:50 +0300

    update DAYS

--- WeatherForYou/app/src/main/java/com/tenevyh/android/weatherforyou/fragments/DaysFragment.kt
+++ WeatherForYou/app/src/main/java/com/tenevyh/android/weatherforyou/fragments/DaysFragment.kt
@@ -5,17 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tenevyh.android.weatherforyou.R
+import com.tenevyh.android.weatherforyou.WeatherApi.MainViewModel
+import com.tenevyh.android.weatherforyou.adapters.WeatherAdapter
+import com.tenevyh.android.weatherforyou.databinding.FragmentDaysBinding
 
 
 class DaysFragment : Fragment() {
+
+    private lateinit var binding: FragmentDaysBinding
+    private lateinit var adapter : WeatherAdapter
+    private val model: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_days, container, false)
+        binding = FragmentDaysBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+        model.liveDataList.observe(viewLifecycleOwner){
+            adapter.submitList(it.subList(1, it.size))
+        }
+    }
+
+    private fun init() = with(binding){
+        adapter = WeatherAdapter()
+        rcView.layoutManager = LinearLayoutManager(activity)
+        rcView.adapter = adapter
     }
 
     companion object {
