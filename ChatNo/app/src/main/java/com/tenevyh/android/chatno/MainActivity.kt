aeca7f1c2df89193d9578e4e1ec84459afbe14ca
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-09-01 23:45:06 +0300

    add a chat emitter

--- ChatNo/app/src/main/java/com/tenevyh/android/chatno/MainActivity.kt
+++ ChatNo/app/src/main/java/com/tenevyh/android/chatno/MainActivity.kt
@@ -2,17 +2,46 @@ package com.tenevyh.android.chatno
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.tenevyh.android.chatno.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val database = Firebase.database
         val myRef = database.getReference("message")
 
-        myRef.setValue("Hello, World!")
+        binding.bSend.setOnClickListener {
+            myRef.setValue(binding.editMessege.text.toString())
+        }
+
+        messageListener(myRef)
+    }
+
+    private fun messageListener(dRef: DatabaseReference){
+        dRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                binding.apply {
+                    rcView.append("\n")
+                    rcView.append("Kir: ${snapshot.value.toString()}")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
