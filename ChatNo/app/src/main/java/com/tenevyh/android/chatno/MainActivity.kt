7f0221a37add841da8b58dd068f8967a812bdecf
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-09-04 01:32:42 +0300

    add sign out

--- ChatNo/app/src/main/java/com/tenevyh/android/chatno/MainActivity.kt
+++ ChatNo/app/src/main/java/com/tenevyh/android/chatno/MainActivity.kt
@@ -1,23 +1,33 @@
 package com.tenevyh.android.chatno
 
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
 import com.tenevyh.android.chatno.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = Firebase.auth
+        setActionBar()
 
         val database = Firebase.database
         val myRef = database.getReference("message")
@@ -27,6 +37,19 @@ class MainActivity : AppCompatActivity() {
         }
 
         messageListener(myRef)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.sign_out){
+            auth.signOut()
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun messageListener(dRef: DatabaseReference){
@@ -43,5 +66,18 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    private fun setActionBar(){
+        val actionBar = supportActionBar
+        Thread{
+            val bitMap = Picasso.get().load(auth.currentUser?.photoUrl).get()
+            val drawable = BitmapDrawable(resources, bitMap)
+            runOnUiThread {
+                actionBar?.setDisplayHomeAsUpEnabled(true)
+                actionBar?.setHomeAsUpIndicator(drawable)
+                actionBar?.title = auth.currentUser?.displayName
+            }
+        }.start()
     }
 }
