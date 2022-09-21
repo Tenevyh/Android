d8bc57641c76849d7c6916748182ca9b086161bf
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-09-21 21:53:17 +0300

    add splash screen

---
+++ MobileLegendsQuiz/app/src/main/java/com/project/android/legend/Activity/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.project.android.legend.Activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.ViewTreeObserver
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+    }
+}
