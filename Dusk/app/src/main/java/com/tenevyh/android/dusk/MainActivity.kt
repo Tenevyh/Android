314bb2ed9e757a9506643c7cfc63e6c3586b23cf
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-10-03 22:11:51 +0300

    add utils

--- Dusk/app/src/main/java/com/tenevyh/android/dusk/MainActivity.kt
+++ Dusk/app/src/main/java/com/tenevyh/android/dusk/MainActivity.kt
@@ -2,10 +2,29 @@ package com.tenevyh.android.dusk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import com.tenevyh.android.dusk.ui.login.ChatAuthStateListener
+import com.tenevyh.android.dusk.ui.utils.replaceFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ChatAuthStateListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showFragment()
+    }
+
+    override fun onAuthStateChanged() {
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            showFragment()
+        }
+    }
+
+    private fun showFragment(){
+        if (FirebaseAuth.getInstance().currentUser == null){
+            replaceFragment(R.id.fragmentContainer, LoginFragment())
+        } else {
+            replaceFragment(R.id.fragmentContainer, ChatLandingFragment())
+        }
     }
 }
---
+++ Dusk/app/src/main/java/com/tenevyh/android/dusk/ui/repository/UserRepository.kt
@@ -0,0 +1,2 @@
+package com.tenevyh.android.dusk.ui.repository
+
