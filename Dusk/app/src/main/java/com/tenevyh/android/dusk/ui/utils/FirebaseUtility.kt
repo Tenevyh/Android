7a1c7cd589995803f49301f4252048218433dc33
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-10-06 21:26:21 +0300

    Update FirebaseUtility.kt

--- Dusk/app/src/main/java/com/tenevyh/android/dusk/ui/utils/FirebaseUtility.kt
+++ Dusk/app/src/main/java/com/tenevyh/android/dusk/ui/utils/FirebaseUtility.kt
@@ -5,5 +5,21 @@ import com.google.firebase.ktx.Firebase
 import com.tenevyh.android.dusk.ui.repository.ChatUser
 
 fun mapFromFirebaseUser(user: FirebaseUser): ChatUser {
-    return ChatUser()
+    val id = user.uid
+    val name = user.displayName ?: ""
+    val lastSeen = System.currentTimeMillis()
+    val photoUrl = user.photoUrl?.toString() ?: ""
+    return ChatUser(id, name, lastSeen, photoUrl)
+}
+
+fun getChatRoot(senderId: String, receiverId: String): String {
+    return if(senderId.compareTo(receiverId) > 1) {
+        "${senderId}_${receiverId}"
+    } else {
+        "${receiverId}_${senderId}"
+    }
+}
+
+interface FetchChatUserListener {
+    fun onFetchUser(chatUser: ChatUser)
 }
