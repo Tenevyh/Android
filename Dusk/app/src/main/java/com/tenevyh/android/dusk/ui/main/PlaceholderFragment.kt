300a1f0d8e41b1303d7a058321fa5137a8cb7fc9
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-10-12 21:31:05 +0300

    Update PlaceholderFragment.kt

--- Dusk/app/src/main/java/com/tenevyh/android/dusk/ui/main/PlaceholderFragment.kt
+++ Dusk/app/src/main/java/com/tenevyh/android/dusk/ui/main/PlaceholderFragment.kt
@@ -1,7 +1,8 @@
 package com.tenevyh.android.dusk.ui.main
 
 import androidx.fragment.app.Fragment
+import com.tenevyh.android.dusk.R
 
-class PlaceholderFragment : Fragment(){
+class PlaceholderFragment : Fragment(R.layout.fragment_main){
 
 }
