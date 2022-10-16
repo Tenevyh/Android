bd153d1f8359803017bfc61b61353e4942f0151f
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-10-16 20:29:32 +0300

    delete binding

--- Dusk/app/src/main/java/com/tenevyh/android/dusk/ui/main/ChatLandingFragment.kt
+++ Dusk/app/src/main/java/com/tenevyh/android/dusk/ui/main/ChatLandingFragment.kt
@@ -6,17 +6,15 @@ import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayoutMediator
 import com.tenevyh.android.dusk.R
 import com.tenevyh.android.dusk.databinding.FragmentChatHomeBinding
+import kotlinx.android.synthetic.main.fragment_chat_home.*
 
 
 class ChatLandingFragment : Fragment(R.layout.fragment_chat_home){
 
-    private lateinit var binding: FragmentChatHomeBinding
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentChatHomeBinding.inflate(layoutInflater)
-        binding.viewPager.adapter = ChatViewPagerAdapter(this)
-        TabLayoutMediator(binding.tabLayout, binding.viewPager) {
+        viewPager.adapter = ChatViewPagerAdapter(this)
+        TabLayoutMediator(tabLayout, viewPager) {
             tab, position -> tab.text = resources.getString(TAB_TITLES[position])
         }.attach()
     }
