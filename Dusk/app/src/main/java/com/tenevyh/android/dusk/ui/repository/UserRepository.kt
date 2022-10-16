d616790d929ab6461d346f86887216fec0e9ee76
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-10-16 19:45:22 +0300

    Update UserRepository.kt

--- Dusk/app/src/main/java/com/tenevyh/android/dusk/ui/repository/UserRepository.kt
+++ Dusk/app/src/main/java/com/tenevyh/android/dusk/ui/repository/UserRepository.kt
@@ -1,8 +1,16 @@
 package com.tenevyh.android.dusk.ui.repository
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
+import com.tenevyh.android.dusk.ui.utils.FetchChatUserListener
+import com.tenevyh.android.dusk.ui.utils.mapFromFirebaseUser
 
 private const val CHAT_USER_TABLE = "users"
 
@@ -16,5 +24,24 @@ class UserRepository{
         val userRef = database.getReference("user_2")
         val userNodeRef = userRef.child(user.uid)
         userNodeRef.setValue(user)
+    }
+
+    fun getCurrentUserByUid(firebaseUser: FirebaseUser, listener: FetchChatUserListener) {
+        val database = Firebase.database
+        val userDbRef = database.getReference("users_2")
+        userDbRef.child(firebaseUser.uid).addListenerForSingleValueEvent(
+            object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val user = snapshot.getValue<ChatUser>()
+                        ?: mapFromFirebaseUser(FirebaseAuth.getInstance().currentUser!!)
+                    listener.onFetchUser(user)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    val user = mapFromFirebaseUser(FirebaseAuth.getInstance().currentUser!!)
+                    listener.onFetchUser(user)
+                }
+            }
+        )
     }
 }
