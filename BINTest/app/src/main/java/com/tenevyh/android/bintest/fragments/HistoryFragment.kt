18dc6cab820943042475cf0f73289b8a898ae42a
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2022-12-29 00:19:30 +0300

    update maket

--- BINTest/app/src/main/java/com/tenevyh/android/bintest/fragments/HistoryFragment.kt
+++ BINTest/app/src/main/java/com/tenevyh/android/bintest/fragments/HistoryFragment.kt
@@ -1,8 +1,21 @@
 package com.tenevyh.android.bintest.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.tenevyh.android.bintest.R
 
-class HistoryFragment: Fragment() {
+class HistoryFragment: Fragment(R.layout.fragment_history) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
     companion object {
         @JvmStatic
--- BINTest/app/src/main/java/com/tenevyh/android/bintest/fragments/RequestFragment.kt
+++ BINTest/app/src/main/java/com/tenevyh/android/bintest/fragments/RequestFragment.kt
@@ -1,8 +1,21 @@
 package com.tenevyh.android.bintest.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.tenevyh.android.bintest.R
 
-class RequestFragment: Fragment() {
+class RequestFragment: Fragment(R.layout.fragment_request) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
     companion object {
         @JvmStatic
