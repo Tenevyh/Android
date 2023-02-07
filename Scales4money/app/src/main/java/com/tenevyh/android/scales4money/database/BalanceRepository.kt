9b68d2ca6b5aa607af20099722ea6bf6e72f5c8e
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2023-02-08 00:12:05 +0300

    Update BalanceRepository.kt

--- Scales4money/app/src/main/java/com/tenevyh/android/scales4money/database/BalanceRepository.kt
+++ Scales4money/app/src/main/java/com/tenevyh/android/scales4money/database/BalanceRepository.kt
@@ -2,6 +2,8 @@ package com.tenevyh.android.scales4money.database
 
 import android.content.Context
 import androidx.room.Room
+import com.tenevyh.android.scales4money.Balance
+import java.util.concurrent.Executors
 
 private const val DATABASE_NAME = "Balances repository"
 
@@ -9,4 +11,28 @@ class BalanceRepository private constructor(context: Context){
     private val database: RequestBalanceDatabase = Room.databaseBuilder(
         context.applicationContext, RequestBalanceDatabase::class.java, DATABASE_NAME)
         .addMigrations(migration_0_1).build()
+
+    private val balanceDao = database.BalanceDao()
+    private val executor = Executors.newSingleThreadExecutor()  //coroutine?
+
+    fun getRequestBalance(balance: Balance){
+        executor.execute{
+            balanceDao.addBalance(balance)
+        }
+    }
+
+    companion object{
+        private var INSTANCE : BalanceRepository? = null
+
+        fun initialize(context: Context){
+            if(INSTANCE == null){
+                INSTANCE = BalanceRepository(context)
+            }
+        }
+
+        fun get(): BalanceRepository{
+            return INSTANCE?: throw java.lang.IllegalStateException(
+                "BalanceRepository must be initialized")
+        }
+    }
 }
