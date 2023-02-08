f072cb02bada985c06fb1f88c7895fb57d12fd6b
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2023-02-08 22:19:38 +0300

    add editBalance

--- Scales4money/app/src/main/java/com/tenevyh/android/scales4money/fragment/BalanceFragment.kt
+++ Scales4money/app/src/main/java/com/tenevyh/android/scales4money/fragment/BalanceFragment.kt
@@ -1,9 +1,19 @@
 package com.tenevyh.android.scales4money.fragment
 
 import androidx.fragment.app.Fragment
+import com.tenevyh.android.scales4money.R
+import kotlinx.android.synthetic.main.balance_fragment.*
 
-class BalanceFragment: Fragment() {
-    companion object{
+class BalanceFragment: Fragment(R.layout.balance_fragment) {
+
+    override fun onStart() {
+        super.onStart()
+        buttonAdd.setOnClickListener {
+            val fragment = SelectionFragment().show(parentFragmentManager, "Selection fragment")
+        }
+    }
+
+    companion object {
         @JvmStatic
         fun newInstance() = BalanceFragment()
     }
---
+++ Scales4money/app/src/main/java/com/tenevyh/android/scales4money/fragment/SelectionFragment.kt
@@ -0,0 +1,15 @@
+package com.tenevyh.android.scales4money.fragment
+
+import androidx.fragment.app.DialogFragment
+import com.tenevyh.android.scales4money.R
+import kotlinx.android.synthetic.main.selection_fragment.*
+
+class SelectionFragment: DialogFragment(R.layout.selection_fragment) {
+
+    override fun onStart() {
+        super.onStart()
+        editBalance.setOnClickListener{
+
+        }
+    }
+}
