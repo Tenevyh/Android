b169a6989196193050fa908f679cabb3ad73a7ee
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2023-02-21 21:26:44 +0300

    Create Limit.kt

---
+++ Scales4money/app/src/main/java/com/tenevyh/android/scales4money/Limit.kt
@@ -0,0 +1,9 @@
+package com.tenevyh.android.scales4money
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity
+data class Limit(@PrimaryKey val date: Date = Date(),
+                 var number: String = "0")
