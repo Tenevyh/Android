d18ca5f63d8042df5ed7abde88d67ba0bde5f544
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2023-03-04 11:22:29 +0300

    Create BalanceViewModel.kt

---
+++ Scales4money/app/src/main/java/com/tenevyh/android/scales4money/viewmodel/BalanceViewModel.kt
@@ -0,0 +1,20 @@
+package com.tenevyh.android.scales4money.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.tenevyh.android.scales4money.Balance
+import com.tenevyh.android.scales4money.Limit
+import com.tenevyh.android.scales4money.database.balance.BalanceRepository
+
+class BalanceViewModel: ViewModel() {
+    private val balanceRepository = BalanceRepository.get()
+    val balanceSheetsLD = balanceRepository.getBalanceSheets()
+    val limitsLD = balanceRepository.getLimits()
+
+    fun addBalance(balance: Balance){
+        balanceRepository.addRequestBalance(balance)
+    }
+
+    fun addLimit(limit: Limit){
+        balanceRepository.addRequestLimit(limit)
+    }
+}
