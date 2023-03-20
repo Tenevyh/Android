6199b39fbd4d31cef67823fecf99c4980ecc8af3
Tenevyh <90172194+Tenevyh@users.noreply.github.com>
2023-03-20 22:20:33 +0300

    add ON buttom limit

--- Scales4money/app/src/main/java/com/tenevyh/android/scales4money/fragment/BalanceFragment.kt
+++ Scales4money/app/src/main/java/com/tenevyh/android/scales4money/fragment/BalanceFragment.kt
@@ -1,16 +1,20 @@
 package com.tenevyh.android.scales4money.fragment
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.View
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.tenevyh.android.scales4money.Balance
+import com.tenevyh.android.scales4money.Limit
 import com.tenevyh.android.scales4money.viewmodel.BalanceViewModel
 import com.tenevyh.android.scales4money.R
 import com.tenevyh.android.scales4money.adapter.BalanceHistoryAdapter
 import kotlinx.android.synthetic.main.balance_fragment.*
+import kotlinx.android.synthetic.main.selection_fragment.*
 import java.text.SimpleDateFormat
 
 class BalanceFragment: Fragment(R.layout.balance_fragment) {
@@ -31,12 +35,28 @@ class BalanceFragment: Fragment(R.layout.balance_fragment) {
                 }
             }
         )
+        balanceViewModel.limitsLD.observe(viewLifecycleOwner,
+        Observer { lastLimits ->
+            lastLimits?.let {
+                if (lastLimits.isNotEmpty()) editLimit.hint = "${lastLimits.last().number} на " +
+                        dateFormat.format(lastLimits.last().date)
+            }
+        })
     }
 
     override fun onStart() {
         super.onStart()
         buttonAdd.setOnClickListener {
             SelectionFragment().show(parentFragmentManager, "Selection fragment")
+        }
+        limitSaveButton.setOnClickListener {
+            if (editLimit.text.isNotEmpty()) {
+                val limit = Limit()
+                limit.number = editLimit.text.toString()
+                balanceViewModel.addLimit(limit)
+            }
+            editLimit.isFocusable = false
+            editLimit.text.clear()
         }
     }
 
